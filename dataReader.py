5e8fd61815094af53b40753fdf2f52f03aca0426
wegoerling <wegoerling@smcm.edu>
2019-07-09 11:17:25 -0400

    test commit

--- dataReader.py
+++ dataReader.py
@@ -3,24 +3,38 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 
 
-# Sorter Method
+# Sorter excel table
 def excel_sorter(file1, sheet1, name1):
     df = pd.read_excel(file1, sheet_name=sheet1)
 
-    print('Before:')
+    print('Before (Unsorted Table):')
     for i in range(9):
         print(df['Unique ID'][i] + ' / ' + df['IT Requestor Name'][i] + ' / ' + df['Purchase Description'][i])
     print(' ')
 
-    finaltable = df[df['IT Requestor Name'] == name1]
+    ftable = df[df['IT Requestor Name'] == name1]
 
-    return finaltable
+    return ftable
 
 
-# implementation
+# print excel table
+def excel_auto_printer():
+    ftable = excel_sorter('Extract 1.xlsx', 'Extract 1', 'Brent, Tipton')
+    for i, u in zip(ftable.index, range(9)):
+        print('After: ')
+        print(
+            ftable['Unique ID'][i] + ' / ' + ftable['IT Requestor Name'][i] + ' / ' + ftable['Purchase Description'][i])
+        print(' ')
+
+
+# print excel table
+def excel_printer(file2, sheet2, name2):
+    ftable = excel_sorter(file2, sheet2, name2)
+    for i, u in zip(ftable.index, range(9)):
+        print('After: ')
+        print(
+            ftable['Unique ID'][i] + ' / ' + ftable['IT Requestor Name'][i] + ' / ' + ftable['Purchase Description'][i])
+        print(' ')
+
+
 print('Showing the first ten entries in the sorted and unsorted tables')
-ftable = excel_sorter('Extract 1.xlsx', 'Extract 1', 'Brent, Tipton')
-print('After: ')
-u = 0
-for i, u in zip(ftable.index, range(9)):
-    print(ftable['Unique ID'][i] + ' / ' + ftable['IT Requestor Name'][i] + ' / ' + ftable['Purchase Description'][i])
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from dataReader import excel_auto_printer
+
 print('Hello Team Drip')
+
+excel_auto_printer()
+
+
 
